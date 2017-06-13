d7d03ca1ddf35b2a4d00bedc667a2fe96f1f992f
polvae <qingguoruanjian@pp.local>
2017-06-13 11:57:23 +0800

    ..
    ..

---
+++ Masony基本使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masony基本使用
+//
+//  Created by KingSoft on 2017/6/12.
+//  Copyright © 2017年 KingSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
